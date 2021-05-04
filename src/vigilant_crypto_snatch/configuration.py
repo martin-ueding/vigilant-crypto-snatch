03096564dd7896f177e606ab2722606a112c7f0e
Martin Ueding <mu@martin-ueding.de>
2021-05-04 19:28:31 +0200

    Take API key from clikraken file

--- src/vigilant_crypto_snatch/configuration.py
+++ src/vigilant_crypto_snatch/configuration.py
@@ -12,13 +12,11 @@ dirs = appdirs.AppDirs(
 )
 config_path = os.path.join(dirs.user_config_dir, "config.yml")
 old_config_path = os.path.expanduser("~/.config/vigilant-crypto-snatch.yml")
+kraken_key_file = os.path.expanduser("~/.config/clikraken/kraken.key")
 
 
 def load_config() -> dict:
-    if os.path.isfile(old_config_path) and not os.path.isfile(config_path):
-        logger.info(f"Moving configuration from {old_config_path} to {config_path}.")
-        os.makedirs(os.path.dirname(config_path), exist_ok=True)
-        shutil.move(old_config_path, config_path)
+    migrate_new_location()
 
     if not os.path.isfile(config_path):
         logger.error(f"Please create the configuration file at {config_path}.")
@@ -26,9 +24,29 @@ def load_config() -> dict:
 
     with open(config_path) as f:
         config = yaml.safe_load(f)
+
+    config = migrate_kraken_key(config)
+
     return config
 
 
 def update_config(config: dict) -> None:
     with open(config_path, "w") as f:
         yaml.dump(config, f)
+
+
+def migrate_new_location():
+    if os.path.isfile(old_config_path) and not os.path.isfile(config_path):
+        logger.info(f"Moving configuration from {old_config_path} to {config_path}.")
+        os.makedirs(os.path.dirname(config_path), exist_ok=True)
+        shutil.move(old_config_path, config_path)
+
+
+def migrate_kraken_key(config: dict) -> dict:
+    if not "kraken" in config and os.path.isfile(kraken_key_file):
+        logger.info("Copying your Kraken API key from clikraken to this config file.")
+        with open(kraken_key_file) as f:
+            key, secret = f.read().strip().split("\n")
+        config["kraken"] = {"key": key, "secret": secret}
+        update_config(config)
+    return config
