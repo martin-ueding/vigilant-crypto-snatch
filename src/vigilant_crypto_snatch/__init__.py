1d40766ef183a40677af2d781285ecb5679fe382
Martin Ueding <mu@martin-ueding.de>
2021-06-18 08:53:07 +0200

    Add basic syslog

--- src/vigilant_crypto_snatch/__init__.py
+++ src/vigilant_crypto_snatch/__init__.py
@@ -1,5 +1,10 @@
 import logging
+import logging.handlers
 
 logger = logging.getLogger("vigilant_crypto_snatch")
+
+h = logging.handlers.SysLogHandler(address="/dev/log")
+h.setFormatter(logging.Formatter("%(name)s %(levelname)s %(message)s"))
+logger.addHandler(h)
 
 __version__ = "4.2.2"
