d448c319a2516c6acd3b87e3270e79c6bfd1b3c0
Martin Ueding <mu@martin-ueding.de>
2021-08-12 15:32:56 +0200

    Export errors

--- src/vigilant_crypto_snatch/marketplace/__init__.py
+++ src/vigilant_crypto_snatch/marketplace/__init__.py
@@ -1,4 +1,7 @@
 from .factory import make_marketplace
+from .marketplace import BuyError
 from .marketplace import check_and_perform_widthdrawal
 from .marketplace import Marketplace
 from .marketplace import report_balances
+from .marketplace import TickerError
+from .marketplace import WithdrawalError
