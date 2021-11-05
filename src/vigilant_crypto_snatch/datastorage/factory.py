4ad97706583c97870abda84e96795527f035292c
Martin Ueding <mu@martin-ueding.de>
2021-11-05 14:21:25 +0100

    Add some log output

--- src/vigilant_crypto_snatch/datastorage/factory.py
+++ src/vigilant_crypto_snatch/datastorage/factory.py
@@ -1,10 +1,10 @@
-from typing import Optional
-
 from . import interface
 from . import sqlalchemy_store
 from .. import configuration
+from .. import logger
 
 
 def make_datastore(persistent: bool) -> interface.Datastore:
     path = configuration.user_db_path if persistent else ""
+    logger.debug(f"Trying to open database at `{path}` …")
     return sqlalchemy_store.SqlAlchemyDatastore(path)
