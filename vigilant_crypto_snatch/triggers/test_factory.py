d9ad1710191aeff3eea6e1b55440cb4339a5517d
Martin Ueding <mu@martin-ueding.de>
2021-12-20 08:23:04 +0100

    Add test with and without fear and greed

--- vigilant_crypto_snatch/triggers/test_factory.py
+++ vigilant_crypto_snatch/triggers/test_factory.py
@@ -16,6 +16,20 @@ def test_dca_trigger() -> None:
         fiat="EUR",
         cooldown_minutes=10,
         volume_fiat=10,
+    )
+    trigger = make_buy_trigger(datastore, source, market, trigger_spec)
+    assert trigger.is_triggered(datetime.datetime.now())
+
+
+def test_fear_greed_trigger() -> None:
+    datastore = ListDatastore()
+    source = MockHistorical()
+    market = MockMarketplace()
+    trigger_spec = TriggerSpec(
+        coin="BTC",
+        fiat="EUR",
+        cooldown_minutes=10,
+        volume_fiat=10,
         fear_and_greed_index_below=101,
     )
     trigger = make_buy_trigger(datastore, source, market, trigger_spec)
