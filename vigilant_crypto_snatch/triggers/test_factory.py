9c0a76b8192b994f6b325092f8e478eaa343e2bb
Martin Ueding <mu@martin-ueding.de>
2021-12-20 08:22:19 +0100

    Add test for factory

---
+++ vigilant_crypto_snatch/triggers/test_factory.py
@@ -0,0 +1,22 @@
+import datetime
+
+from ..datastorage import ListDatastore
+from ..historical import MockHistorical
+from ..marketplace import MockMarketplace
+from ..triggers import make_buy_trigger
+from .interface import TriggerSpec
+
+
+def test_dca_trigger() -> None:
+    datastore = ListDatastore()
+    source = MockHistorical()
+    market = MockMarketplace()
+    trigger_spec = TriggerSpec(
+        coin="BTC",
+        fiat="EUR",
+        cooldown_minutes=10,
+        volume_fiat=10,
+        fear_and_greed_index_below=101,
+    )
+    trigger = make_buy_trigger(datastore, source, market, trigger_spec)
+    assert trigger.is_triggered(datetime.datetime.now())
