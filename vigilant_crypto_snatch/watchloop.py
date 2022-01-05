9c2160915e623c8be99b0b76f9a33f0024f55b8a
Martin Ueding <mu@martin-ueding.de>
2022-01-05 17:16:51 +0100

    Log HTTP errors only as warnings

--- vigilant_crypto_snatch/watchloop.py
+++ vigilant_crypto_snatch/watchloop.py
@@ -53,7 +53,7 @@ def process_trigger(trigger: Trigger):
         if trigger.has_cooled_off(now) and trigger.is_triggered(now):
             trigger.fire(now)
     except HttpRequestError as e:
-        notify_and_continue(e, logging.ERROR)
+        notify_and_continue(e, logging.WARNING)
     except TickerError as e:
         notify_and_continue(e, logging.ERROR)
     except BuyError as e:
