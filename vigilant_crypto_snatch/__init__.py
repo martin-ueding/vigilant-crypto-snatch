3f44f4bcf0d8b288e92de205fe0a7889ea8bb8ff
Martin Ueding <mu@martin-ueding.de>
2022-01-16 17:00:33 +0100

    Only add syslog handler when on Linux

--- vigilant_crypto_snatch/__init__.py
+++ vigilant_crypto_snatch/__init__.py
@@ -1,10 +1,12 @@
 import logging.handlers
+import platform
 
 logger = logging.getLogger("vigilant_crypto_snatch")
 
-h = logging.handlers.SysLogHandler(address="/dev/log")
-h.setFormatter(logging.Formatter("%(name)s %(levelname)s %(message)s"))
-h.setLevel(logging.INFO)
-logger.addHandler(h)
+if platform.system() == "Linux":
+    h = logging.handlers.SysLogHandler(address="/dev/log")
+    h.setFormatter(logging.Formatter("%(name)s %(levelname)s %(message)s"))
+    h.setLevel(logging.INFO)
+    logger.addHandler(h)
 
 __version__ = "5.4.0"
