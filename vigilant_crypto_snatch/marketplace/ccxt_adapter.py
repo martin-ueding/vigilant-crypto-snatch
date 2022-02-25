894c935f440eaadaab2d4ba3da7875a60b04f7cd
Martin Ueding <mu@martin-ueding.de>
2022-02-25 13:28:06 +0100

    Implement spot price and ticker with CCXT

--- vigilant_crypto_snatch/marketplace/ccxt_adapter.py
+++ vigilant_crypto_snatch/marketplace/ccxt_adapter.py
@@ -1,4 +1,6 @@
 import datetime
+from typing import Dict
+from typing import List
 from typing import Type
 
 import ccxt
@@ -13,12 +15,19 @@ class CCXTMarketplace(Marketplace):
     def __init__(self, config: CCXTConfig):
         exchange_type: Type[ccxt.Exchange] = getattr(ccxt, config.exchange)
         self.exchange = exchange_type(config.parameters)
+        self.markets = self.exchange.load_markets()
 
     def place_order(self, asset_pair: AssetPair, volume: float) -> None:
-        pass
+        volume_coin = (
+            volume / self.get_spot_price(asset_pair, datetime.datetime.now()).last
+        )
+        self.exchange.create_market_order(
+            symbol=get_symbol(self.markets, asset_pair), side="buy", amount=volume_coin
+        )
 
     def get_spot_price(self, asset_pair: AssetPair, now: datetime.datetime) -> Price:
-        pass
+        response = self.exchange.fetch_ticker(get_symbol(self.markets, asset_pair))
+        return response["last"]
 
     def get_name(self) -> str:
         return f"{self.exchange.name} via CCXT"
@@ -32,3 +41,13 @@ class CCXTMarketplace(Marketplace):
 
     def withdrawal(self, coin: str, volume: float) -> None:
         pass
+
+
+def get_symbol(markets: Dict[str, Dict], asset_pair: AssetPair) -> str:
+    for market in markets.values():
+        if market["base"] == asset_pair.coin and market["quote"] == asset_pair.fiat:
+            return market["symbol"]
+    else:
+        raise RuntimeError(
+            f"Could not find asset pair {asset_pair} among the available markets via CCXT."
+        )
